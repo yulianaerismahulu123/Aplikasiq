c19f16713f56b72bb98e98357b345b48835173c0
yulianaerismahulu123 <yulianaerismanhulu@gmail.com>
2025-02-21 05:26:13 -0800

    Add files via upload

---
+++ room.php
@@ -0,0 +1,46 @@
+<?php include "header.php";?>
+  
+		<div class="container">
+		<div class="row">
+			<div class="col-md-6">	
+				<div class="jumbotron">
+				  <h1><b>HARGA RUMAH</b></h1>
+				</div>
+				<div class="container">
+		       <div class="row">
+			   <div class="col-md-3">
+				<h3>FASILITAS RUMAH KE 1</h3>
+				<img src="images/family.jpg" height="90" width="400">
+				<h4>RP 2.5000.000</h4>
+				 <a href="room-presidential.php" class="btn btn-primary">Lihat Detail</a>
+				<a href="data-tamu.php" class="btn btn-primary">Book Now</a>
+			</div>
+			<div class="col-md-3">
+				<h3>RUMAH KE 2</h3>
+				<img src="images/delux.jpg" height="90" width="400">
+				<h4> RP 4.500.000</h4>
+				 <a href="room-delux.php" class="btn btn-primary">Lihat Detail</a>
+				<a href="data-tamu.php" class="btn btn-primary">Book Now</a>
+			</div>
+		<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<h3>RUMAH KE 3</h3>
+				<img src="images/rasa.jpg" height="90" width="400">
+				<h4> Rp 2.100.000</h4>
+				 <a href="room-standart.php " class="btn btn-primary">Lihat Detail</a>
+				<a href="data-tamu.php" class="btn btn-primary">Book Now</a>
+			</div>
+		<div class="container">
+		<div class="row">
+			<div class="col-md-3">
+				<h3>RUMAH KE 4</h3>
+				<img src="images/family.jpg" height="90" width="400">
+				<h4> RP 3.200.000</h4>
+				 <a href="room-family.php" class="btn btn-primary">Lihat Detail</a>
+				<a href="data-tamu.php" class="btn btn-primary">Book Now</a>
+			</div>
+		</div>
+	</div>
+
+	<?php include "footer.php"; ?>
