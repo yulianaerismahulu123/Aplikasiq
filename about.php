148007185205e4149cf23f9a0cea6e3b2755a0ce
yulianaerismahulu123 <yulianaerismanhulu@gmail.com>
2025-02-21 05:18:18 -0800

    Add files via upload

---
+++ about.php
@@ -0,0 +1,44 @@
+<body>
+  <div class="container">
+    <h1>TENTANG RUMAH JUALAN</h1>
+    <form>
+      <div class="form-group">
+        <label for="nama">Nama</label>
+        <input type="text" class="form-control" id="nama" placeholder="Masukkan nama Anda">
+      </div>
+      <div class="form-group">
+        <label for="email">Alamat</label>
+        <input type="email" class="form-control" id="alamat" placeholder="Masukkan alamat Anda">
+      </div>
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" class="form-control" id="email" placeholder="Masukkan email Anda">
+      </div>
+      <div class="form-group">
+        <label for="telepon">Nomor Telepon</label>
+        <input type="tel" class="form-control" id="telepon" placeholder="Masukkan nomor telepon Anda">
+      </div>
+      <div class="form-group">
+        <label for="destinasi">Nomor Rumah</label>
+        <select class="form-control" id="destinasi">
+          <option>Pilih NO Rumah</option>
+          <option>rumah 01</option>
+          <option>rumah 02</option>
+          <option>rumah 03</option>
+          <option>rumah 04</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="tanggal">Tanggal</label>
+        <input type="date" class="form-control" id="tanggal">
+      </div>
+      <button type="submit" class="btn btn-primary">Pesan</button>
+    </form>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</body>
+
+</html>
