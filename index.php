50fa745b2e15fcf827daaab23076e6cd8737d2a8
yulianaerismahulu123 <yulianaerismanhulu@gmail.com>
2025-02-21 05:20:04 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,104 @@
+<?php include "header.php"; ?>
+
+	<div class="jumbotron">
+    <h1 class="display-5">SELAMAT DATANG DI APLIKASI PENJUALAN RUMAH</h1>
+    <p class="lead">FASILITAS RUMAH.</p>
+  </div>
+
+		<!-- Awal script Slider/ Carousel -->
+		<div id="contoh-carousel" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#contoh-carousel" data-slide-to="0" class="active">LINGKUNGAN RUMAH</li>
+        <li data-target="#contoh-carousel" data-slide-to="1">ISI DALAM RUMAH</li>
+        <li data-target="#contoh-carousel" data-slide-to="2">DEPAN RUMAH</li>
+      </ol>
+
+      <div class="carousel-inner" role="listbox">
+			<!-- Awal script Slider pertama -->
+        <div class="item active">
+          <img src="images/lingkungan.jpg" alt="Berisi keterangan gambar" style="width:50%; height: 500px;">
+          <div class="carousel-caption">
+            <h1>TEMPAT RUMAH JUALAN</h1>
+            <h4>DI nias selatan</h4>
+            <p> LINGKUNGAN RUMAH</p>
+          </div>
+          </div>
+    </div>
+		<!-- Awal script Button Geser Kiri dan Kanan -->
+    <a class="left carousel-control" href="#contoh-carousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+    </a>
+		
+    <a class="right carousel-control" href="#contoh-carousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+    </a><!-- Akhir script Button Geser Kiri dan Kanan -->
+		
+    </div><!-- Akhir script Slider/Carousel -->
+		
+		<!-- Awal Jumbotron -->
+		<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+					<div class="jumbotron">
+						<h2>LOKASI RUMAH</h2>
+						<p> Halo Selamat Datang DI tempat rumah ini, semoga anda DI rumah baru anda tempat yang Nyaman , Bersih, Murah, Dan Berkulaitas untuk para langganan</p>
+						
+
+				</div>
+      </div>
+		</div>
+		</div><!-- Akhir Jumbotron -->
+		
+		<!-- Awal Page -->
+		<div class="container">
+		<!-- Awal Panel -->
+		<div class="row">
+			<div class="panel panel-default">
+				<div class="panel-body">
+				<h2 style="text-muted"><span class="glyphicon glyphicon-list"></span> </h2>
+				
+        <p></p>
+				
+				<div class="row">
+					<div class="col-md-6">
+					<h3>FASILITAS LINGKUNGAN RUMAH</h3>
+						<img src="images/rumah.jpg" class="img-thumbnail img-responsive" width="50%">
+						<p><br/><a class="btn btn-danger btn-xs" href="restoran.php"role="button">Selengkapnya</a></p>
+					</div>
+					<div class="col-md-6">
+					<h3>ISI DALAM RUMAH</h3>
+						<img src="images/isi rumah.jpg" class="img-thumbnail img-responsive" width="50%">
+						<p>.<br/><a class="btn btn-info btn-xs" href="galeri permainan.php"role="button">jelajahi</a></p>
+					</div>
+				</div>
+				</div>
+      </div>
+
+		</div><!-- Akhir Panel -->
+		<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">
+					<div class="jumbotron">
+						<h1>fasilitas kamar dan ruang tamu</h1>
+						<div class="container">
+		       <div class="row">
+			   <div class="col-md-4">
+				<h3>FASILITAS ISI KAMAR KE RUANG 1</h3>
+				<img src="images/president.jpg" height="200" width="200">
+			</div>
+			<div class="col-md-4">
+				<h3>FASILITAS ISI KAMAR KE RUANG 2</h3>
+				<img src="images/delux.jpg" height="200" width="200">
+			</div>
+			<div class="col-md-4">
+				<h3>FASILITAS RUANG TAMU</h3>
+				<img src="images/ruang tamu.jpg" height="200" width="200">
+			</div>
+		      </div>
+		      </div>
+		      </div>
+		      </div>
+		
+<?php include "footer.php"; ?>
