caf4e62d708600cb1e73389c6eddd8ac20404d50
yulianaerismahulu123 <yulianaerismanhulu@gmail.com>
2025-02-21 05:19:41 -0800

    Add files via upload

---
+++ header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+  <head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>APLIKASI RUMAH JUALAN</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+		<link href="bootstrap/css/dataTables.bootstrap.min.css" rel="stylesheet">
+		<link href="bootstrap/css/styles.css" rel="stylesheet">
+		<style>
+      body {
+        padding-top: 60px;
+      }
+		</style>
+  </head>
+  <body>
+    
+	<!-- Awal script Navbar -->
+    <nav class="navbar navbar-default navbar-fixed-top" id="scrollspy">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle Nav</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php">INFORMASI PEMILIK JUALAN RUMAH</a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="index.php"><span class="glyphicon glyphicon-home"></span> HOME <span class="sr-only">(current)</span></a>
+						</li>
+            <li>
+							<a href="about.php"><span class="glyphicon glyphicon-star"></span> ABOUT</a>
+						</li>
+            <li>
+							<a href="contact.php"><span class="glyphicon glyphicon-envelope"></span> INFORMASI RUMAH</a>
+						</li>
+            <li>
+							<a href="room.php"><span class="glyphicon glyphicon-bed"></span> ROOM</a>
+						</li>
+						C
+						</li>
+						<li>
+							<a href="data-tamu.php"><span class="glyphicon glyphicon-ok"></span> WILAYAH</a>
+						</li>
+						<li>
+
+							<a href="login.php"><span class="glyphicon glyphicon-lock"></span> LOGIN</a>
+						</li>
+          </ul>
+        </div>
+      </div>
+    </nav><!-- Akhir script Navbar -->
